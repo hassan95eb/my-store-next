1d1e06da919c7a223bd4a2cf434e7d84a8af8d8d
Hassan Amini <108970826+hassan95eb@users.noreply.github.com>
2025-12-02 23:35:35 +0330

    use json server

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,14 +1,25 @@
-import ClientCompo from "@/components/ClientCompo";
-import ServerComponent from "@/components/ServerComponent";
+import axios from "axios";
 import React from "react";
 
-export default function About() {
+interface IData {
+  id: string;
+  title: string;
+  views: number;
+}
+async function About() {
+  const res = await axios.get("http://localhost:8000/posts");
+  const data = await res.data;
+  console.log(data);
   return (
     <div>
-      this is about page
-      <ClientCompo>
-        <ServerComponent />
-      </ClientCompo>
+      {data.map((item: IData) => {
+        return (
+          <div className="bg-amber-200 my-2 text-black" key={item.id}>
+            {item.title}
+          </div>
+        );
+      })}
     </div>
   );
 }
+export default About;
